e6d76df4f99644181dfbbff5fb1fbdafbb2fe4c0
rtock1 <ryan@tocks.org>
2021-12-09 21:59:41 -0800

    pre day 10

--- src/AOC_2021/day10/Part1.java
+++ src/AOC_2021/day10/Part1.java
@@ -5,7 +5,47 @@ import AOC_Helper_Classes.*;
 public class Part1 {
     public static void main(String[] args){
         String data = ReadInput.getString("AOC_2021.day10");
-        //String[] d = data.split("\n");
-        System.out.println(data);
+        String[] d = data.split("\n");
+        int total = 0;
+        for (String c:d){
+            total += corrupted(c);
+        }
+        System.out.println(total);
+    }
+    public static boolean openClose(char c){
+        switch (c){
+            case '(':
+            case '[':
+            case '{':
+            case '<':
+                return true;
+            default:
+                return false;
+        }
+    }
+    public static int corrupted(String c){
+        char[] opens = new char[100];
+        int current = 0;
+        for (String a:c.split("")){
+            if (openClose(a.charAt(0))){
+                opens[current] = a.charAt(0);
+                current++;
+            } else {
+                current--;
+                if ((opens[current] =='(' && a.charAt(0) != ')') || (opens[current] =='[' && a.charAt(0) != ']') || (opens[current] =='{' && a.charAt(0) != '}') || (opens[current] =='<' && a.charAt(0) != '>')){
+                    switch (a.charAt(0)){
+                        case ')':
+                            return 3;
+                        case ']':
+                            return 57;
+                        case '}':
+                            return 1197;
+                        case '>':
+                            return 25137;
+                    }
+                }
+            }
+        }
+        return 0;
     }
 }
--- src/AOC_2021/day10/Part2.java
+++ src/AOC_2021/day10/Part2.java
@@ -2,10 +2,93 @@ package AOC_2021.day10;
 
 import AOC_Helper_Classes.*;
 
+import java.math.BigInteger;
+import java.util.Arrays;
+
 public class Part2 {
     public static void main(String[] args){
         String data = ReadInput.getString("AOC_2021.day10");
-        //String[] d = data.split("\n");
-        System.out.println(data);
+        String[] d = data.split("\n");
+        BigInteger[] numbers = new BigInteger[d.length];
+        int counter = 0;
+        for (String c:d) {
+            BigInteger curr = corrupted(c);
+            if (!curr.equals(new BigInteger("0"))){
+                numbers[counter] = corrupted(c);
+                counter++;
+            }
+        }
+        BigInteger[] numbers2 = new BigInteger[counter];
+        for (int a = 0;a<counter;a++){
+            numbers2[a] = numbers[a];
+        }
+        Arrays.sort(numbers2);
+        //System.out.println(Arrays.toString(numbers2));
+        System.out.println(numbers2[numbers2.length/2]);
+    }
+    public static boolean openClose(char c){
+        switch (c){
+            case '(':
+            case '[':
+            case '{':
+            case '<':
+                return true;
+            default:
+                return false;
+        }
+    }
+    public static BigInteger corrupted(String c){
+        char[] opens = new char[100];
+        int current = 0;
+        for (String a:c.split("")){
+            //System.out.println(Arrays.toString(opens));
+            if (openClose(a.charAt(0))){
+                opens[current] = a.charAt(0);
+                current++;
+            } else {
+                current--;
+                if ((opens[current] =='(' && a.charAt(0) != ')') || (opens[current] =='[' && a.charAt(0) != ']') || (opens[current] =='{' && a.charAt(0) != '}') || (opens[current] =='<' && a.charAt(0) != '>')){
+                    return new BigInteger("0");
+                }
+                opens[current] = '.';
+            }
+        }
+        BigInteger total = new BigInteger("0");
+
+        int len = 0;
+        for (char i:opens){
+            if (i=='(' || i=='[' || i=='{' || i== '<'){
+                len++;
+            }
+        }
+        for (int i=len-1;i>=0;i--){
+            //System.out.println(opens[i]);
+            switch (opens[i]){
+                case '(':
+                    //System.out.println(")");
+                    total = total.multiply(new BigInteger("5"));
+                    total = total.add(new BigInteger("1"));
+                    break;
+                case '[':
+                    //System.out.println("]");
+                    total = total.multiply(new BigInteger("5"));
+                    total = total.add(new BigInteger("2"));
+                    break;
+                case '{':
+                    //System.out.println("}");
+                    total = total.multiply(new BigInteger("5"));
+                    total = total.add(new BigInteger("3"));
+                    break;
+                case '<':
+                    //System.out.println(">");
+                    total = total.multiply(new BigInteger("5"));
+                    total = total.add(new BigInteger("4"));
+                    break;
+                default:
+                    break;
+            }
+        }
+        //System.out.println(total);
+        return total;
     }
 }
