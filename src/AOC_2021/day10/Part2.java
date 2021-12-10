31f9fd0d09196c12a121c745383eee4fa7272cca
rtock1 <ryan@tocks.org>
2021-12-09 20:55:12 -0800

    pre day 10

---
+++ src/AOC_2021/day10/Part2.java
@@ -0,0 +1,11 @@
+package AOC_2021.day10;
+
+import AOC_Helper_Classes.*;
+
+public class Part2 {
+    public static void main(String[] args){
+        String data = ReadInput.getString("AOC_2021.day10");
+        //String[] d = data.split("\n");
+        System.out.println(data);
+    }
+}
